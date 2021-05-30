9e6e8573f50e341234f371c9215284bec64b17c4
Graeme McRobert <graeme.mcrobert@hotmail.co.uk>
2021-05-30 08:00:58 +0100

    Add main page and Nav
    Signed-off-by: Graeme McRobert <graeme.mcrobert@hotmail.co.uk>

--- src/App.js
+++ src/App.js
@@ -1,15 +1,53 @@
-import React, { Component } from "react";
+import React, {useState} from "react";
 import "@patternfly/react-core/dist/styles/base.css";
 import "@patternfly/patternfly/patternfly.css";
+import {
+    Nav,
+    NavItem,
+    NavList,
+    Page,
+    PageHeader,
+    PageSection,
+    PageSectionVariants
+} from '@patternfly/react-core';
 
 import GoalSetting from './pages/GoalSetting';
 
-export default class App extends Component {
-    render() {
-        return (
-            <div className="early-learning-goals">
-                <GoalSetting />
-            </div>
-        );
-    }
+const TopNav = () => {
+
+    const [activeItem, setActiveItem] = useState(0);
+
+    const onSelect = result => setActiveItem(result.itemId);
+
+    return (
+        <Nav onSelect={onSelect}>
+            <NavList>
+                <NavItem id="default-link1" to="#default-link1" itemId={0} isActive={activeItem === 0}>
+                    Link 1
+                </NavItem>
+                <NavItem id="default-link2" to="#default-link2" itemId={1} isActive={activeItem === 1}>
+                    Link 2
+                </NavItem>
+                <NavItem id="default-link3" to="#default-link3" itemId={2} isActive={activeItem === 2}>
+                    Link 3
+                </NavItem>
+                <NavItem id="default-link4" to="#default-link4" itemId={3} isActive={activeItem === 3}>
+                    Link 4
+                </NavItem>
+            </NavList>
+        </Nav>
+    );
 }
+
+export const App = () => {
+
+    const Header = <PageHeader topNav={<TopNav />}/>;
+
+    return (
+        <Page header={Header}>
+            <PageSection variant={PageSectionVariants.light}>
+                <GoalSetting/>
+            </PageSection>
+        </Page>
+    )
+};
